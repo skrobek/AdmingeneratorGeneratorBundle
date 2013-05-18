e29581bc042412dc68e6337d2def45dfc9bf0fac
Skrobek <pskrobek@gmail.com>
2013-05-19 01:36:26 +0200

    add $.browser support

--- Resources/public/js/bootstrap/bootstrap-typeahead-extended.js
+++ Resources/public/js/bootstrap/bootstrap-typeahead-extended.js
@@ -19,6 +19,49 @@
  * limitations under the License.
  * ============================================================ */
 
+(function( jQuery, window, undefined ) {
+"use strict";
+
+var matched, browser;
+
+jQuery.uaMatch = function( ua ) {
+  ua = ua.toLowerCase();
+
+  var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+    /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+    /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+    /(msie) ([\w.]+)/.exec( ua ) ||
+    ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+    [];
+
+  return {
+    browser: match[ 1 ] || "",
+    version: match[ 2 ] || "0"
+  };
+};
+
+// Don't clobber any existing jQuery.browser in case it's different
+if ( !jQuery.browser ) {
+  matched = jQuery.uaMatch( window.navigator.userAgent );
+  browser = {};
+
+  if ( matched.browser ) {
+    browser[ matched.browser ] = true;
+    browser.version = matched.version;
+  }
+
+  // Chrome is Webkit, but Webkit is also Safari.
+  if ( browser.chrome ) {
+    browser.webkit = true;
+  } else if ( browser.webkit ) {
+    browser.safari = true;
+  }
+
+  jQuery.browser = browser;
+}
+
+})( jQuery, window );
+
 
 !function($){
 
@@ -51,7 +94,7 @@
   , select: function () {
       var val = this.$menu.find('.active').attr('data-value'),
       item = this.getter(val)
-      
+
       this.$element
           .val(this.updater(item))
           .change()
@@ -61,15 +104,15 @@
   , updater: function (item) {
       return item
     }
-    
-  , getter: function (pk) {      
+
+  , getter: function (pk) {
       var source = $.isFunction(this.source) ? this.source() : this.source
-        
+
       return $.grep(source, function(item){
           return item == pk
       })[0]
   }
-  
+
   , setter: function(item) {
       return item;
   }
